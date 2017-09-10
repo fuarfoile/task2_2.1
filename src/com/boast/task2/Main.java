edfb7026087b56ab39997d12f105862c1ba2bd01
Oleh Bondarenko <boa.st@ya.ru>
2017-09-10 19:39:04 +0300

    upd

--- src/com/boast/task2/Main.java
+++ src/com/boast/task2/Main.java
@@ -8,31 +8,12 @@
 package com.boast.task2;
 
 import com.boast.textEditor.*;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        StringBuilder text = getText();
+        StringBuilder text = TextEditor.getText();
 
         TextEditor.showAllFinds(text, "(\\+\\d+)\\$%(\\+\\d+)");
-    }
-
-    private static StringBuilder getText() {
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("Type a text: (double enter to finish)");
-        String strLine = in.nextLine();
-
-        StringBuilder text = new StringBuilder(strLine);
-
-        do {
-            strLine = in.nextLine();
-            if(strLine.length() > 0) {
-                text = new StringBuilder(text + "\n" + strLine);
-            }
-        } while (strLine.length() > 0);
-
-        return text;
     }
 }
--- src/com/boast/textEditor/TextEditor.java
+++ src/com/boast/textEditor/TextEditor.java
@@ -7,10 +7,30 @@
 
 package com.boast.textEditor;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TextEditor {
+
+    public static StringBuilder getText() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Type a text: (double enter to finish)");
+        String strLine = in.nextLine();
+
+        StringBuilder text = new StringBuilder(strLine);
+
+        do {
+            strLine = in.nextLine();
+            if(strLine.length() > 0) {
+                text = new StringBuilder(text + "\n" + strLine);
+            }
+        } while (strLine.length() > 0);
+
+        return text;
+    }
+
     public static void showAllFinds(StringBuilder text, String searchPattern) {
         Pattern pattern = Pattern.compile(searchPattern);
         Matcher matcher = pattern.matcher(text);
